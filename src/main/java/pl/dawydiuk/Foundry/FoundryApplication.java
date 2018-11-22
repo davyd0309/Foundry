17754f6a761bf34075f7a4cee1d98880d8320d53
Konrad Dawydiuk <Konrad.Dawydiuk.ctr@sabre.com>
2018-11-22 14:25:33 +0100

    init commit

---
+++ src/main/java/pl/dawydiuk/Foundry/FoundryApplication.java
@@ -0,0 +1,12 @@
+package pl.dawydiuk.Foundry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoundryApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/dawydiuk/Foundry/FoundryApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.dawydiuk.Foundry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoundryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
